9c834ec5a78df6fe4204659933a8abc36e6dfb5c
Mark Peters <mapeters101@earthlink.net>
2024-11-02 17:59:44 -0400

    Lesson173:  Changed code to use num.

--- alternative_method_with_num.py
+++ alternative_method_with_num.py
@@ -12,19 +12,18 @@ dt=0.02 # [s]
 t=np.arange(t0,t_end+dt,dt)
 y=1000*t
 
-
 frame_amount=len(t)
 
-def update_plot(t):
-    print(t)
-    wire_1.set_data([-160,0],[-100,1000*t])
-    wire_2.set_data([160,0],[-100,1000*t])
-    y_text.set_text('y = '+str(int(1000*t))+' mm')
+def update_plot(num):
+    print(num)
+    wire_1.set_data([-160,0],[-100,y[num]])
+    wire_2.set_data([160,0],[-100,y[num]])
+    y_text.set_text('y = '+str(y[num])+' mm')
 
     return wire_1,wire_2,y_text
 
 # Define figure properties
-fig=plt.figure(figsize=(16,9),dpi=120,facecolor=(0.8,0.8,0.8))
+fig=plt.figure(figsize=(16,9),dpi=80,facecolor=(0.8,0.8,0.8))
 gs=gridspec.GridSpec(4,4)
 plt.subplots_adjust(left=0.03,bottom=0.035,right=0.99,top=0.97,wspace=0.15,hspace=0.2)
 ax=fig.add_subplot(gs[:,0:4],facecolor=(0.9,0.9,0.9))
@@ -36,8 +35,6 @@ plate_A=ax.plot([-50,50],[-200,-200],'g',linewidth=200)
 box_object=dict(boxstyle='square',fc=(0.9,0.9,0.9),ec='r',lw=1)
 y_text=ax.text(400,250,'',size=20,color='r',bbox=box_object)
 
-copyright=ax.text(-580,250,'© Mark Misin Engineering',size=15)
-
 plt.xlim(-600,600)
 plt.ylim(-300,300)
 plt.xlabel('x-length [mm]',fontsize=12)
@@ -46,5 +43,5 @@ plt.grid()
 
 
 engine_ani=animation.FuncAnimation(fig,update_plot,
-    frames=t,interval=20,repeat=False,blit=True)
+    frames=frame_amount,interval=20,repeat=False,blit=True)
 plt.show()
